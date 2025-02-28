0c6db807f2348b7f035c5fddedc378a9e546bf2a
alejandrodomingueztorres <85324176+alejandrodomingueztorres@users.noreply.github.com>
2025-02-28 10:00:43 -0500

    Update ImplementacionEmergencia.java

--- src/views/ImplementacionEmergencia.java
+++ src/views/ImplementacionEmergencia.java
@@ -18,7 +18,7 @@ public class ImplementacionEmergencia extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/wiews/FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
